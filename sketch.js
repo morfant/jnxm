865559f663ab3509d3c0a6aad8674cb0b2fe5382
giy <giy.hands@gmail.com>
2018-02-12 12:52:02 +0900

    bin printing

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var playing = false;
 var completion;
 var input, button, greeting;
 var textLength = 0;
+var binName = "";
+var binText;
 
 function setup() {
   // createCanvas(320, 180);
@@ -30,6 +32,11 @@ function setup() {
 
   textAlign(CENTER);
   textSize(50);
+
+  binText = createElement('h2');
+  binText.position(20, 200);
+  binText.id("binText");
+
 }
 
 function draw() {
@@ -37,6 +44,10 @@ function draw() {
   // imageMode(CENTER);
   image(vid, 0, 0); // set video position
   var t = selectAll('.textField');
+
+  // console.log(binName);
+  binText.html(binName);
+
 }
 
 function greet() {
@@ -65,44 +76,79 @@ function mousePressed() {
   }
 }
 
+function keyTyped() {
+
+  if (keyCode === ENTER) {
+    var str = select('.textField').elt.value;
+    textLength = str.length;
+    console.log(textLength);
+    console.log("enter!");
+    console.log("value: " + str);
+    // korToUnicode();
+    binName = text2Binary(str);
+    console.log("binName: " + binName);
+
+  } else if (keyCode === 32) {
+    console.log("sp");
+  }
+
+}
+
 // full screen
 function keyPressed() {
   if (key === 'f' || key === 'F') {
     var fs = fullscreen();
     fullscreen(!fs);
   }
-  else if (keyCode === ENTER) {
-    textLength = select('.textField').elt.value.length;
-    console.log(textLength);
-  	console.log("enter!");
-    console.log(select('.textField').elt.value);
-    korToUnicode();
-
-  }
 }
 
-function replaceAll(strTemp, strValue1, strValue2){ 
-	console.log(strTemp);
-  while(1){
-    if( strTemp.indexOf(strValue1) != -1 )
-      strTemp = strTemp.replace(strValue1, strValue2);
-    else
-      break;
-  }
-  return strTemp;
+
+// function replaceAll(strTemp, strValue1, strValue2){ 
+// 	console.log(strTemp);
+//   while(1){
+//     if( strTemp.indexOf(strValue1) != -1 )
+//       strTemp = strTemp.replace(strValue1, strValue2);
+//     else
+//       break;
+//   }
+//   return strTemp;
+// }
+
+// function unicodeToKor(){
+//   //유니코드 -> 한글
+//   var str=document.getElementById("a").value;
+//   document.getElementById("b").value=unescape(replaceAll(str, "\\", "%"));
+// }
+
+// function korToUnicode(){
+//   //한글 -> 유니코드
+//   var str = select('.textField').elt.value;
+//   // var rslt = escape(replaceAll(str, "\\", "%"));
+//   var rslt = escape(str);
+
+//   console.log("escaped: " + rslt);
+//   var unicodes = specialCharRemove(rslt);
+//   console.log("specialchar removed: " + unicodes);
+//   arr_unicode = unicodes.split("u");
+//   console.log("splited: " + arr_unicode);
+
+// }
+
+function text2Binary(string) {
+  return string.split('').map(function (char) {
+      return char.charCodeAt(0).toString(2);
+  }).join(' ');
 }
 
-function unicodeToKor(){
-  //유니코드 -> 한글
-  var str=document.getElementById("a").value;
-  document.getElementById("b").value=unescape(replaceAll(str, "\\", "%"));
-}
+// function specialCharRemove(str) {
+//   var tstr = str;
+//   var pattern = /[^(a-zA-Z0-9)]/gi;   // 특수문자 제거
+   
+//   //var pattern = /[^(0-9)]/gi;   // 숫자이외는 제거
+   
+//   if(pattern.test(tstr)){
+//     tstr = tstr.replace(pattern,"");
+//   }
+//   return tstr;
+// }
 
-function korToUnicode(){
-  //한글 -> 유니코드
-  var str = select('.textField').elt.value;
-  var rslt = escape(replaceAll(str, "\\", "%"));
-  console.log(rslt);
-
-//  document.getElementById("bb").value=escape(replaceAll(str, "\\", "%"));
-}
