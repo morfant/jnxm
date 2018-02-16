b6c69ace2d44e84ba76d8cf43a93384c7bacd1de
giy <giy.hands@gmail.com>
2018-02-16 17:58:45 +0900

    h2 -> text, animation

--- sketch.js
+++ sketch.js
@@ -1,41 +1,30 @@
-
-var vid;
 var playing = false;
-var completion;
 var input, button, greeting;
-var textLength = 0;
+var charLength = 0;
+var binNum = 0;
 var binName = "";
+var binNamePrinted = false;
 var binText;
 var inputPosX = 450;
 var inputPosY = 500;
+var prevEvent = 0;
+var greet = "넥슨컴퓨터박물관에 오신 것을 환영합니다.\n\n이름을 입력하세요."
+var greeting_posY = 200;
+var enterInput = false;
+var textVelY = 20;
 
 function setup() {
+ 
+  frameRate(30); // Attempt to refresh at starting FPS
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.style('display', 'block');
 
-  // video
-  // vid = createVideo("data/test.mp4");
-  // vid.size(800, 600); 
-  // vid.hide();
-
-  // text input
-  greeting = createElement('h2', '넥슨컴퓨터박물관에 오신 것을 환영합니다. </br> </br> 이름을 입력하세요.');
-  greeting.position(300, 80);
-  greeting.class("greeting-text");
-
+  // Text Input 
   input = createInput();
-  input.position(inputPosX, inputPosY);
   input.attribute('maxlength', '4');
-  input.class("textField"); // see data/style.css
+  input.class("text-field"); // see data/style.css
 
-  // submit button
-  // button = createButton('submit');
-  // button.position(input.x + input.width, 165);
-  // button.mousePressed(greet); // function to run when click submit
-
-  textAlign(CENTER);
-  textSize(50);
-
+  // Binary result text
   binText = createElement('h2');
   binText.position(inputPosX, inputPosY + 200);
   binText.id("binText");
@@ -44,50 +33,59 @@ function setup() {
 
 function draw() {
   background(89, 89, 89);
-  // imageMode(CENTER);
-  // image(vid, 0, 0); // set video position
-  var t = selectAll('.textField');
 
-  // console.log(binName);
-  binText.html(binName);
+  // greeting text
+  fill(250);
+  textAlign(CENTER);
+  textSize(50);
 
-}
+  if (enterInput) {
+    var name = input.value();
+    input.remove();
 
-function greet() {
-  var name = input.value();
-  greeting.html('hello '+name+'!');
-  input.value('');
-
-  for (var i=0; i<200; i++) {
     push();
-    fill(random(255), 255, 255);
-    translate(random(width), random(height));
-    rotate(random(2*PI));
+    translate(width/2, greeting_posY + 300);
     text(name, 0, 0);
     pop();
-  }
-}
 
-function mousePressed() {
-  if (!playing) {
-    vid.play();
-    playing = true;
+    greeting_posY -= textVelY;
+
+    // phase of animation
+    if (greeting_posY < -150) {
+      textVelY = 0;
+
+      if (!binNamePrinted) {
+        for (var i = 0; i < binNum; i++) {
+          console.log(binName[i]);
+          text(binName[i], 100 + i * 10, 600);
+        }
+        binNamePrinted = true;
+      }
+
+    }
   }
-  else {
-    vid.pause();
-    playing = false;
-  }
+
+  push();
+  translate(width/2, greeting_posY);
+  text(greet, 0, 0);
+  pop();
+
+  // console.log(binName);
+  // binText.html(binName);
+
 }
 
 function keyTyped() {
 
   if (keyCode === ENTER) {
-    var str = select('.textField').elt.value;
-    textLength = str.length;
-    console.log(textLength);
-    console.log("enter!");
-    console.log("value: " + str);
-    // korToUnicode();
+    enterInput = true;
+
+    var str = input.value();
+    charLength = str.length;
+
+    analyzeText(str);
+    console.log("binNum: " + binNum);
+
     binName = text2Binary(str);
     console.log("binName: " + binName);
 
@@ -95,6 +93,21 @@ function keyTyped() {
     console.log("sp");
   }
 
+}
+
+function analyzeText(string) {
+  string.split('').map(function (char) {
+    var b = char.charCodeAt(0).toString(2).length;
+    binNum += b;
+  });
+}
+
+
+
+function text2Binary(string) {
+  return string.split('').map(function (char) {
+      return char.charCodeAt(0).toString(2);
+  }).join(' ');
 }
 
 // full screen
@@ -105,56 +118,10 @@ function keyPressed() {
   }
 }
 
-
-// function replaceAll(strTemp, strValue1, strValue2){ 
-// 	console.log(strTemp);
-//   while(1){
-//     if( strTemp.indexOf(strValue1) != -1 )
-//       strTemp = strTemp.replace(strValue1, strValue2);
-//     else
-//       break;
-//   }
-//   return strTemp;
-// }
-
-// function unicodeToKor(){
-//   //유니코드 -> 한글
-//   var str=document.getElementById("a").value;
-//   document.getElementById("b").value=unescape(replaceAll(str, "\\", "%"));
-// }
-
-// function korToUnicode(){
-//   //한글 -> 유니코드
-//   var str = select('.textField').elt.value;
-//   // var rslt = escape(replaceAll(str, "\\", "%"));
-//   var rslt = escape(str);
-
-//   console.log("escaped: " + rslt);
-//   var unicodes = specialCharRemove(rslt);
-//   console.log("specialchar removed: " + unicodes);
-//   arr_unicode = unicodes.split("u");
-//   console.log("splited: " + arr_unicode);
-
-// }
-
-function text2Binary(string) {
-  return string.split('').map(function (char) {
-      return char.charCodeAt(0).toString(2);
-  }).join(' ');
-}
-
-// function specialCharRemove(str) {
-//   var tstr = str;
-//   var pattern = /[^(a-zA-Z0-9)]/gi;   // 특수문자 제거
-   
-//   //var pattern = /[^(0-9)]/gi;   // 숫자이외는 제거
-   
-//   if(pattern.test(tstr)){
-//     tstr = tstr.replace(pattern,"");
-//   }
-//   return tstr;
-// }
-
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+function mousePressed() {
+}
+
